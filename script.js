501e4115f11faf49299b8f6a99a4a2d01c40bc0b
Michael Pimlott <pettingbutterscotch@gmail.com>
2016-02-23 15:55:54 -0800

    first board Javascript

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-// Your JS goes here
+
+var target = document.getElementsByTagName('body')[0];
+
+for (var i = 0; i < 63; i++){
+  var div = document.createElement('div');
+  div.style.float = 'left';
+  div.style.width = '11.1%';
+  div.style.paddingBottom = '11.1%';
+  if (i % 2 ===0){
+    div.style.backgroundColor = 'black';
+  }else {
+    div.style.backgroundColor = 'red';
+  }
+  target.appendChild(div);
+}
+
+
+
+
+//if (i % 2===0){
+//backgroundColor = "red";
+//} else {
+document.getElementById('div').style.backgroundColor = "black";
