61b0517a102f8887881f18aa25916bcd72bab86f
Michael Pimlott <pettingbutterscotch@gmail.com>
2016-02-24 18:07:31 -0800

    add random colors and lint

--- script.js
+++ script.js
@@ -1,23 +1,16 @@
+(function() {
+  'use strict';
+  var target = document.getElementsByTagName('body')[0];
 
-var target = document.getElementsByTagName('body')[0];
+  for (var i = 0; i < 63; i++) {
+    var div = document.createElement('div');
+    var color = '#' + Math.floor(Math.random() * 16777215).toString(16);
 
-for (var i = 0; i < 63; i++){
-  var div = document.createElement('div');
-  div.style.float = 'left';
-  div.style.width = '11.1%';
-  div.style.paddingBottom = '11.1%';
-  if (i % 2 ===0){
-    div.style.backgroundColor = 'black';
-  }else {
-    div.style.backgroundColor = 'red';
+    div.style.float = 'left';
+    div.style.width = '11.1%';
+    div.style.paddingBottom = '11.1%';
+    div.style.backgroundColor = color;
+
+    target.appendChild(div);
   }
-  target.appendChild(div);
-}
-
-
-
-
-//if (i % 2===0){
-//backgroundColor = "red";
-//} else {
-document.getElementById('div').style.backgroundColor = "black";
+})();
